e70c6763064352b8732913225e9bdfbf2a6f9fe9
Git-DmR <dm.r@mail.uk>
2022-10-31 19:16:55 +0300

    Lesson 11. templates

--- d4f/news/views.py
+++ d4f/news/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import News
+
 def index(request):
-    #print(dir(request))
-    return HttpResponse("<H2>Hello World !!!</H2>")
+    news = News.objects.all()
+    result = '<h1>List of news.</h1>\n'
+    for item in news:
+        result += f"<div>\n<h2>{item.title}:<h2>\n<p>{item.content}</p>\n</div></hr>"
+
+    return HttpResponse(result)
 
 def test(request):
     return HttpResponse("<H1>Test page!!</H1>")
