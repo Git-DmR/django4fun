34e9bd987815306c556b07543116dfa56edcd00c
Git-DmR <dm.r@mail.uk>
2022-10-31 17:56:18 +0300

    Add News models

--- d4f/d4f/urls.py
+++ d4f/d4f/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+
 
 from news.views import index , test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('news.urls')),
+    path('', include('news.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
