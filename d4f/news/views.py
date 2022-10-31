b12ac7e79f25f864d6095c2e033af3ad4c1aca1a
Git-DmR <dm.r@mail.uk>
2022-10-31 17:01:52 +0300

    Add news.urls.

--- d4f/news/views.py
+++ d4f/news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    #print(dir(request))
+    return HttpResponse("<H2>Hello World !!!</H2>")
 
-# Create your views here.
+def test(request):
+    return HttpResponse("<H1>Test page!!</H1>")
