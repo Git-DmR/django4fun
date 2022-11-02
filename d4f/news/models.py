497ae3080594990089658cf410329babe2bcadcb
Git-DmR <dm.r@mail.uk>
2022-11-02 18:58:16 +0300

    Add admin castomization. Lesson 12

--- d4f/news/models.py
+++ d4f/news/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%y/%m/%d')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name="Наименование")
+    content = models.TextField(verbose_name="Содержание", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата содания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата публикации")
+    photo = models.ImageField(upload_to='photos/%y/%m/%d', verbose_name="Изображение", blank=True)
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано" )
 
     def __str__(self):
         return f'{self.title} model'
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = "Новости"
+        ordering = ['-created_at', "title"]
