d5d7cd97edfe23d998ef5b90bcb3152931c818ab
Git-DmR <dm.r@mail.uk>
2022-11-11 01:23:01 +0300

    Lesson 22. Add method for absolute url.

--- d4f/news/models.py
+++ d4f/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class News(models.Model):
@@ -9,6 +10,9 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%y/%m/%d', verbose_name="Изображение", blank=True)
     is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
     category = models.ForeignKey('Category', verbose_name="Категория", on_delete=models.PROTECT, null=True)
+
+    def get_absolute_url(self):
+        return reverse_lazy('view_news',kwargs={"news_id": self.pk})
 
     def __str__(self):
         return f'{self.title} model'
@@ -21,6 +25,10 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name="Название категории")
+
+    def get_absolute_url(self):
+        return reverse_lazy('category',kwargs={"category_id": self.pk})
+
 
     def __str__(self):
         return f'{self.title.upper()}'
