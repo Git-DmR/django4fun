f6e1cce1d2f0317ae9e4202ea709b7f47b57b0a0
Git-DmR <dm.r@mail.uk>
2022-11-18 13:47:26 +0300

    Lesson 28. Add ListView in category.

--- d4f/news/urls.py
+++ d4f/news/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, get_category, view_news, add_news, HomeNews
+from .views import HomeNews, NewsByCategory, view_news, add_news
+
 
 urlpatterns = [
-    # path('', index, name='home'),
     path('', HomeNews.as_view(), name='home'),
-    path('newscat/<int:category_id>/', get_category, name='category'),
+    path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
     path('news/<int:news_id>', view_news, name='view_news'),
     path('news/add_news', add_news, name='add_news'),
 ]
--- d4f/news/views.py
+++ d4f/news/views.py
@@ -19,24 +19,21 @@ class HomeNews(ListView):
     def get_queryset(self):
         return News.objects.filter(is_published=True)
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news' : news,
-        'title' : 'Список новостей.',
-            }
-    return render(request, "news/index.html", context)
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    # category = Category.objects.get(pk=category_id)
-    category = get_object_or_404(Category, pk=category_id)
-    context = {
-        'news': news,
-        'category': category,
-    }
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
 
-    return render(request, "news/category.html", context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'],  is_published=True)
+
 
 def view_news(request, news_id):
     #news_item = News.objects.get(pk=news_id)
@@ -54,13 +51,13 @@ def add_news(request):
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
 
-# def add_news(request):
-#     if request.method == 'POST':
-#         form = NewsForm(request.POST)
-#         if form.is_valid():
-#             # print(form.cleaned_data)
-#             News.objects.create(**form.cleaned_data)
-#             return redirect("home")
-#     else:
-#         form = NewsForm()
-#     return render(request, 'news/add_news.html', {'form': form})
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            News.objects.create(**form.cleaned_data)
+            return redirect("home")
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
