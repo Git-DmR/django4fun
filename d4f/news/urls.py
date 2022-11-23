7d2c509adaf86f8325826694df4f9b7f323fac54
Git-DmR <dm.r@mail.uk>
2022-11-23 20:46:43 +0300

    Lesson 45. Add registration.

--- d4f/news/urls.py
+++ d4f/news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import HomeNews, NewsByCategory, ViewSingleNews, CreateNews
+from .views import HomeNews, NewsByCategory, ViewSingleNews, CreateNews, register, login
 
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
--- d4f/news/views.py
+++ d4f/news/views.py
@@ -1,8 +1,30 @@
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 from .models import News, Category
 from .forms import NewsForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Вы успешно зарегистрировались.")
+            return redirect('login')
+        else:
+            messages.error(request, "Ошибка регистрации.")
+    else:
+        form = UserCreationForm()
+        messages.error(request, "Ошибка регистрации.")
+
+    return render(request, 'news/register.html', {"form" : form})
+
+def login(request):
+    return render(request, 'news/login.html')
 
 
 class HomeNews(ListView):
